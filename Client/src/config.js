d5bfbf10ae027256320b0b8c03ff1047de6b55ff
Pradeep kumar <117191675+Techi-pradeep@users.noreply.github.com>
2023-06-16 22:11:24 +0530

    Update config.js---added Cyclic url

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: "http://localhost:8080",
+  BASE_URL: "https://giddy-attire-bass.cyclic.app",
+  // BASE_URL: "http://localhost:8080",
 };
 
 // BASE_URL: "https://my-api.com",
