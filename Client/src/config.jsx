8f53f21ec25947de5fe0847f91825799c675898b
Techi-pradeep <pradeepk259909@gmail.com>
2023-06-20 18:01:44 +0530

    updated project for making it live

--- Client/src/config.jsx
+++ Client/src/config.jsx
@@ -1,10 +1,10 @@
  const API_CONFIG = {
   /*  mern project used locally--Development phase ---PORT= Same Backend PORT */
-  BASE_URL: "http://localhost:8000/api",
+  // BASE_URL: "http://localhost:8000/api",
 
     /*   Deploying backend  on cyclic or render  */
   // BASE_URL: "https://giddy-attire-bass.cyclic.app",
-  // BASE_URL: "https://bookstore-v34j.onrender.com/",
+  BASE_URL: "https://bookstore-v34j.onrender.com/api",  /**api keyword matching backend routes */
 };
 
 export {API_CONFIG};
